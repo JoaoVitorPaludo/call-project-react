bcf6fcd6fbcc46b3c6e207e60c6ef4a8d1ff899a
JoaoVitorPaludo <joaovpaludo@gmail.com>
2024-03-06 12:50:30 -0300

    feat: Implementando estrutura inicial do banco de dados prisma

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,22 +1,22 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Text, TextInput } from "@ignite-ui/react";
-import { ArrowRight } from "phosphor-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Form, FormAnnotation } from "./styles";
-import { useRouter } from "next/router";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnnotation } from './styles'
 
 const claimUsernameFormSchema = z.object({
   username: z
     .string()
-    .min(3, { message: "O usuário precisa ter pelo menos 3 letras " })
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras ' })
     .regex(/^([a-z\\-]+)$/i, {
-      message: "O usuário pode ter apenas letras e hifens ",
+      message: 'O usuário pode ter apenas letras e hifens ',
     })
     .transform((username) => username.toLowerCase()),
-});
+})
 
-type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 export function ClaimUsernameForm() {
   const {
     register,
@@ -24,13 +24,13 @@ export function ClaimUsernameForm() {
     formState: { errors },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
-  });
+  })
 
-  const router = useRouter();
+  const router = useRouter()
   async function handleClaimUsername(data: ClaimUsernameFormData) {
-    const { username } = data;
+    const { username } = data
 
-    await router.push(`/register?username=${username}`);
+    await router.push(`/register?username=${username}`)
   }
   return (
     <>
@@ -40,7 +40,7 @@ export function ClaimUsernameForm() {
           prefix="ignite.com/"
           placeholder="seu-usuário"
           crossOrigin="anonymous"
-          {...register("username")}
+          {...register('username')}
         />
         <Button size="sm" type="submit">
           Reservar
@@ -51,9 +51,9 @@ export function ClaimUsernameForm() {
         <Text size="sm">
           {errors.username
             ? errors.username?.message
-            : "Digite o nome do usuário"}
+            : 'Digite o nome do usuário'}
         </Text>
       </FormAnnotation>
     </>
-  );
+  )
 }
